86274969f70fbf92cfbcd06e7d31de68dda52612
Dilruba Showkat <dilrubashowkat@gmail.com>
2017-10-23 18:22:56 -0500

    array created

--- Documents/NetBeansProjects/JavaPractice/src/javapractice/JavaPractice.java
+++ Documents/NetBeansProjects/JavaPractice/src/javapractice/JavaPractice.java
@@ -14,8 +14,20 @@ public class JavaPractice {
     /**
      * @param args the command line arguments
      */
+    public static void sum_two(int array[], int number){
+        int result = 0;
+        
+   
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        int nums [] = {5,6,3,4,2,9,10,34,56};
+        int make = 7;
+        sum_two(nums,make);
+        
+        
+        
     }
     
 }
